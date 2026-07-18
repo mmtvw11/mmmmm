59ae89e7f5f2b874aaf7a783ea357f2af9ef622e
mmtvw11 <mammatovv10@gmail.com>
2026-07-18 22:58:38 +0600

    Исправить pubspec.yaml и main.dart - добавить все зависимости

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'core/di/service_locator.dart';
+import 'core/theme/app_theme.dart';
+import 'features/auth/presentation/bloc/auth_bloc.dart';
+import 'features/auth/presentation/pages/login_page.dart';
+import 'features/auth/presentation/pages/splash_page.dart';
+import 'features/tasks/presentation/bloc/tasks_bloc.dart';
+import 'features/tasks/presentation/pages/tasks_page.dart';
+
+void main() {
+  setupServiceLocator();
+  runApp(const MyApp());
+}
 
 class MyApp extends StatefulWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   State<MyApp> createState() => _MyAppState();
@@ -10,9 +23,30 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (_) => getIt<AuthBloc>()),
+        BlocProvider(create: (_) => getIt<TasksBloc>()),
+      ],
+      child: MaterialApp(
+        title: 'TaskHub',
+        debugShowCheckedModeBanner: false,
+        theme: AppTheme.lightTheme,
+        darkTheme: AppTheme.darkTheme,
+        themeMode: ThemeMode.system,
+        home: BlocBuilder<AuthBloc, AuthState>(
+          builder: (context, state) {
+            if (state is AuthInitial) {
+              return const SplashPage();
+            } else if (state is AuthAuthenticated) {
+              return const TasksPage();
+            } else if (state is AuthUnauthenticated) {
+              return const LoginPage();
+            }
+            return const SplashPage();
+          },
+        ),
+      ),
+    );
   }
 }
-
-
-
